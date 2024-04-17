4e7d5394a14716664d8de0963b8f1e58fdca9083
Renan Souza <renan@klever.io>
2024-04-17 20:10:45 +0100

    create documentation and code refactor

--- src/test/java/com/premium/minds/GameTest.java
+++ src/test/java/com/premium/minds/GameTest.java
@@ -1,3 +1,5 @@
+package com.premium.minds;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -23,7 +25,7 @@ public class GameTest {
 
 
     @Test
-    public void shoud_CatchPokemons_When_CorrectMoveIsPassed() {
+    public void should_CatchPokemons_When_CorrectMoveIsPassed() {
         // given
         String[] nextMoves = {"", "E", "e", "NESO", "nEsO", "NSNSNSNSNS", "NsNsNSnsNS", "EEENNNOENENENOOESSS"};
         int[] expected = {1, 2, 2, 4, 4, 2, 2, 16};
@@ -39,18 +41,32 @@ public class GameTest {
     }
 
     @Test
+    public void should_CatchOnTime_When_PassedInTheSamePosition() {
+        // given
+        String[] nextMoves = {"sss", "sssnnn"};
+        int expected = 4;
+
+        // when
+        int actual;
+        for (String nextMove : nextMoves) {
+            actual = new Game().catchPokemons(nextMove);
+
+            // then
+            assertEquals(actual, expected);
+        }
+    }
+
+    @Test
     public void should_ThrowIllegalArgumentException_When_InvalidMove() {
         // given
         String[] nextMoves = {"a", "1", "Q", "NE1", "NESOA", " N", "E ", "1NE", "NE#", "@NES", "NES."};
 
         // when
-        int actual;
         for (String nextMove : nextMoves) {
             Executable executable = () -> game.catchPokemons(nextMove);
 
             // then
             assertThrows(IllegalArgumentException.class, executable);
         }
-
     }
 }
